23d1b8756d26f2ece73b9a6bbbfd835490a68b48
yukuda <leeyookoo@naver.com>
2021-10-04 23:04:15 +0900

    JpaMain

--- jpashop1/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop1/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -16,6 +19,10 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
+
+            OrderItem orderItem = new OrderItem();
 
             tx.commit();    // flush
         } catch (Exception e) {
